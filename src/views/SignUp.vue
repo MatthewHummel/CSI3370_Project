ff13461942b6bbef9e48502e17e27f13509730c1
MatthewHummel <matthew2hummel@gmail.com>
2023-03-31 14:03:44 -0400

    Created sign up form

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -7,6 +7,18 @@
         Super generic sign up!
     </p>
 
+    <div class="register">
+        <input type="text" placeholder="Enter Name" />
+        <input type="text" placeholder="Enter Email" />
+        <input type="password" placeholder="Enter Password" />
+        <button>Sign Up Now</button>
+    </div>
+
+
+
+
+    <!-- HEADER -->
+
     <div class="main-desktop-header1">
         <img
           src="/playground_assets/background1922-iww.svg"
@@ -32,7 +44,7 @@
         </div>
       </div>
     
-
+      <!-- FOOTER -->
 
       <div class="main-desktop-footer">
         <div class="main-desktop-group14">
@@ -126,9 +138,31 @@ export default {
     padding-top: 8vw;
 }
 
+.register {
+    padding-top: 2vw;
+}
 
+.register input {
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid skyblue;
+}
 
-
+.register button {
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    background-color: green;
+    color: white;
+    cursor: pointer;
+    margin: 0 auto;
+    display: block;
+}
 
 
 .main-desktop-header1 {
