8db024dc7fafdf77a84e8d3449c7d773aee8c15d
MatthewIsrailOU <MatthewIsrail@oakland.edu>
2023-04-03 08:14:29 -0400

    .

--- src/models/data-models.js
+++ src/models/data-models.js
@@ -43,15 +43,15 @@ var schema = {
 function readTable (table, cb) {
     let sql = `SELECT * FROM ${table}`;
     let data = {};
-    db.all(sql, function(err, rows) {        /* Return all results of query */
-      if (err) throw(err);            /* If there's an error, terminate app */
-      rows.forEach(function(row) {       /* For each row matching the query */
-        data[row.id] = {};                  /* init row id as top-level key */
-        Object.keys(row).forEach(function(k) {    /* For each column of row */
-          data[row.id][k] = unescape(row[k]);     /* add the key-value pair */
+    db.all(sql, function(err, rows) {        // Return all results of query 
+      if (err) throw(err);            // If there's an error, terminate app 
+      rows.forEach(function(row) {       // For each row matching the query 
+        data[row.id] = {};                  // init row id as top-level key 
+        Object.keys(row).forEach(function(k) {    // For each column of row 
+          data[row.id][k] = unescape(row[k]);     // add the key-value pair 
         });
       });
-      cb(data);    /* data = { id: { "colname" : value }, ... }, id2: ... } */
+      cb(data);    // data = { id: { "colname" : value }, ... }, id2: ... } 
     });
 };
   
@@ -61,12 +61,12 @@ function createRow (table, cb) {
 };
   
 function updateRow (table, rb, cb) {
-    var pairs = "";           /* for constructing 'identifier = value, ...' */
-    for (field of schema[table].slice(1)) {   /* for every column except id */
-        if (pairs) pairs += ", ";    /* insert comma unless string is empty */
-        pairs += `${field} = '${escape(rb[field])}'`;   /* column = 'value' */
+    var pairs = "";           // for constructing 'identifier = value, ...'
+    for (field of schema[table].slice(1)) {   //for every column except id
+        if (pairs) pairs += ", ";    // insert comma unless string is empty 
+        pairs += `${field} = '${escape(rb[field])}'`;   //
     }
-    let sql = `UPDATE ${table} SET ${pairs} WHERE id = ?`;  /* ? = rb['id'] */
+    let sql = `UPDATE ${table} SET ${pairs} WHERE id = ?`;  // ? = rb['id']
     db.run(sql, rb['id'], cb);
 };
   
