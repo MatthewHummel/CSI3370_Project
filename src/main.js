f5a5d27d29a1968c6682a5518f124bca587fe035
MatthewHummel <matthew2hummel@gmail.com>
2023-04-06 12:43:26 -0400

    commented main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import LottieVuePlayer from '@lottiefiles/vue-lottie-player'
-
+//firebase specific imports
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
@@ -13,7 +13,7 @@ Vue.use(LottieVuePlayer)
 Vue.config.productionTip = false
 
 
-// Your web app's Firebase configuration
+// The web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBAxYk2h3QLpGDZwYEC73KaQjXPHXDB0UY",
   authDomain: "csi-3370-website.firebaseapp.com",
@@ -22,17 +22,12 @@ const firebaseConfig = {
   messagingSenderId: "253186344751",
   appId: "1:253186344751:web:fa03ee90f4a7c36b6ec3da"
 };
-
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-
 // use these for db and auth
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
-
 export { auth, db };
-
-
 
 
 new Vue({
